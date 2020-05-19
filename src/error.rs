8ad0b6cf0a89ba1f267d14237ad2985c292180b2
AlephAlpha <alephalpha911@gmail.com>
2020-05-19 16:44:14 +0800

    :bookmark: v0.1.2

--- src/error.rs
+++ src/error.rs
@@ -1,26 +1,12 @@
 //! Errors that can be returned when parsing a pattern file.
 
-use std::{
-    error,
-    fmt::{self, Display, Formatter},
-};
+use thiserror::Error;
 
 /// Errors that can be returned when parsing a pattern file.
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, Error, PartialEq)]
 pub enum Error {
-    /// Unexpected byte.
+    #[error("Unexpected byte: {0:#x}.")]
     UnexpectedByte(u8),
-    /// apgcode not encoded in extended Wechsler format.
+    #[error("apgcode not encoded in extended Wechsler format")]
     Unencodable,
 }
-
-impl Display for Error {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::UnexpectedByte(c) => write!(f, "Unexpected byte: {:#x}", c),
-            Error::Unencodable => write!(f, "apgcode not encoded in extended Wechsler format."),
-        }
-    }
-}
-
-impl error::Error for Error {}
