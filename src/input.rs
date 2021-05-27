0e2cb44a5897a73c9ce178e98dc716eaa009b3af
AlephAlpha <alephalpha911@gmail.com>
2021-05-27 20:51:43 +0800

    :cat: 给更多类型实现 Input

--- src/input.rs
+++ src/input.rs
@@ -6,7 +6,7 @@ use std::{
 
 /// Types that can be passed to parsers as input.
 ///
-/// The trait is implemented for [`&str`](str) and [`BufReader`].
+/// The trait is implemented for `&str`, `&[u8]` and [`BufReader`].
 /// When parsing a file, you can take a [`BufReader<File>`] as input.
 pub trait Input {
     /// An iterator over lines of the input.
@@ -62,8 +62,44 @@ impl<'a> Input for Lines<'a> {
     }
 }
 
+impl<'a> Input for &'a [u8] {
+    type Lines = IoLines<&'a [u8]>;
+    type Line = String;
+    type Bytes = IntoIter<u8>;
+
+    fn lines(self) -> Self::Lines {
+        BufRead::lines(self)
+    }
+
+    fn line(item: <Self::Lines as Iterator>::Item) -> Result<Self::Line, Error> {
+        item
+    }
+
+    fn bytes(line: Self::Line) -> Self::Bytes {
+        line.into_bytes().into_iter()
+    }
+}
+
 impl<R: Read> Input for BufReader<R> {
     type Lines = IoLines<BufReader<R>>;
+    type Line = String;
+    type Bytes = IntoIter<u8>;
+
+    fn lines(self) -> Self::Lines {
+        BufRead::lines(self)
+    }
+
+    fn line(item: <Self::Lines as Iterator>::Item) -> Result<Self::Line, Error> {
+        item
+    }
+
+    fn bytes(line: Self::Line) -> Self::Bytes {
+        line.into_bytes().into_iter()
+    }
+}
+
+impl<'a, B: BufRead> Input for &'a mut B {
+    type Lines = IoLines<&'a mut B>;
     type Line = String;
     type Bytes = IntoIter<u8>;
 
